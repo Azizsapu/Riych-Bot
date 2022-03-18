2d9c7c6ab9a42a3280237615569680b9be35609b
AzizXDBOT <92648503+Azizsapu@users.noreply.github.com>
2022-03-18 19:06:31 +0700

    settings

---
+++ config.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// self or public
+global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
+
+// setting
+global.ownername ="AzizXdBOT"
+global.ownernumber = "6289518527362"
+global.botname = "ZeizBotXd"
+global.thumbnail = fs.readFileSync("./settings/Riych.jpg") //sesuaikan dengan nama foto
+global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
+global.lolkey = 'RIFQIBOTZ' //apikey
+global.limit = {
+		free:20,
+		premium:1000
+	}
+global.session_name = "session.json"
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
